a2b51f4391de78f6ea815cb182f924da078bc062
kotmari <mariannakot79@gmail.com>
2026-06-19 23:08:31 +0300

    Add user and message models, implement MongoDB connection, and integrate Clerk middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,29 @@
-import express from 'express';
-import 'dotenv/config'
+import express from "express";
+import "dotenv/config";
+import {clerkMiddleware} from "@clerk/express"
+import cors from "cors"
 
-const app = express()
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`))
+
+import User from "./models/user.model.js"
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL
+
+app.use(express.json())
+app.use(cors({
+   origin: FRONTEND_URL,
+   credentials: true
+}))
+
+app.use(clerkMiddleware())
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is up and running on port ${PORT}`);
+});
