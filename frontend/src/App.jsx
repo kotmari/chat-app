ce3e0379f51cef19817a0d676eb0bb4a76ffc745
kotmari <mariannakot79@gmail.com>
2026-06-26 16:53:16 +0300

    feat: add theme and wallpaper context providers
    - Implemented ThemeProvider to manage light/dark themes and theme presets.
    - Created WallpaperProvider to manage wallpaper selection and storage.
    - Added utility functions for theme and wallpaper management.
    - Introduced theme presets for consistent styling across the application.
    - Created initial AuthPage and ChatPage components.
    - Added CSS for theme presets to style the application based on user preferences.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,45 @@
-
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
-import './App.css'
+import { Routes, Route, Navigate } from "react-router";
+import { ThemeProvider } from "./context/ThemeContext";
+import { WallpaperProvider } from "./context/WallpaperContext";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
 
-
+  if(!isLoaded) return <p>loading...</p>
   return (
-    <div>
-      <h1>My app</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-
-    </div>
-  )
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />
+            }
+          />
+          <Route
+            path="/auth"
+            element={
+              !isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />
+            }
+          />
+        </Routes>
+        {/* <h1 className="text-7xl text-red-600">My app</h1>
+        <Button>My click</Button>
+        <header>
+          <Show when="signed-out">
+            <SignInButton mode="modal" />
+            <SignUpButton mode="modal" />
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </header> */}
+      </WallpaperProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/context/wallpaper.js
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+
+
+export const WallpaperContext = createContext(null)
+
+export function useWallpaper() {
+   const ctx = useContext(WallpaperContext)
+
+  if(!ctx){
+      throw new Error("useWallpaper must be used within WallpaperProvider")
+   }
+
+   return ctx
+}
